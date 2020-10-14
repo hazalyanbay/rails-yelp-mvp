fb7310bd3c7056065a3425093f9d02beceb7568b
Hazal Yanbay buse.xezal@gmail.com <buse.xezal@gmail.com>
2020-10-14 15:31:33 +0300

    yey

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,41 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new # needed to instantiate the form_for
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+
+    def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
+    end
+
+  end
+
+
+  private
+
+  def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
